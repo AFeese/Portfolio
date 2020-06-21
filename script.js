e4c17ad9118982869b7a5f95e2c45a624345de3b
AFeese <58886725+AFeese@users.noreply.github.com>
2020-06-21 18:08:23 -0400

    added back to top button

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $('.sidenav').sidenav();
-    $('.materialboxed').materialbox();
-    $('.parallax').parallax();
-  });
+  $('.sidenav').sidenav();
+  $('.materialboxed').materialbox();
+  $('.parallax').parallax();
+});
+
+//Scroll to top button
+var mybutton = document.getElementById("topBtn");
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+};
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
